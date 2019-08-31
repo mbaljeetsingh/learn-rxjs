f421b845b4bfc0a394bfac8f307e695a7e553b1e
mbaljeetsingh <bsinghs.007@gmail.com>
2019-08-31 22:11:35 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -1,12 +1,30 @@
-import { of } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { interval, of } from 'rxjs';
+import { buffer, take, bufferCount, bufferTime } from 'rxjs/operators';
 
 console.clear()
-const source = of('World').pipe(
-  map(x => `Hello ${x}!`)
-);
+/* buffer Operator */
+// const ob = interval(100)
+//   .pipe(
+//     buffer(interval(1000)),
+//     take(3)
+//   )
 
-source.subscribe(
+/* bufferCount Operator */
+// const ob = of(1,2,3).pipe(
+//   bufferCount(2)
+// )
+// const ob = of(1,2,3).pipe(
+//   bufferCount(2,1)
+// )
+
+/* bufferTime Operator */
+const ob = interval(1000)
+  .pipe(
+    take(6),
+    bufferTime(2000, 1000)
+  )
+
+ob.subscribe(
   x => console.log(x),
   e => console.log('error:', e),
   () => console.log('complete')
