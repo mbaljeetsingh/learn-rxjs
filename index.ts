ac83de9592526a028c4112978febf0c85dbd0faa
mbaljeetsingh <bsinghs.007@gmail.com>
2019-08-31 21:22:24 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -1,9 +1,13 @@
-import { of } from 'rxjs'; 
+import { of } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-
+console.clear()
 const source = of('World').pipe(
   map(x => `Hello ${x}!`)
 );
 
-source.subscribe(x => console.log(x));
+source.subscribe(
+  x => console.log(x),
+  e => console.log('error:', e),
+  () => console.log('complete')
+);
